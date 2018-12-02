fbc61184387b987705a6b7402776751a61f013e8
lobhasramatkar8888 <31283767+lobhasramatkar8888@users.noreply.github.com>
2018-12-02 15:35:22 +0530

    Add files via upload

---
+++ learn2.js
@@ -0,0 +1,21 @@
+function f1() {
+
+document.getElementById("ex10").innerHTML =  " hello javascript" ;							// body...
+}
+
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('ex11').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
